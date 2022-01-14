1b3b1a0790cb24c8433453b8a75fd9aa28df8247
Wallace Delesposti <wallace.delesposti@gmail.com>
2022-01-14 15:33:04 -0300

    Adição da API com interação do mapa

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -4,8 +4,12 @@ import MapView, { Marker, Callout } from "react-native-maps";
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
 
+import api from '../services/api';
+
 function Main({ navigation }) {
+    const [devs, setDevs] = useState([]);
     const [currentRegion, setCurrentRegion] = useState(null);
+    const [techs, setTechs] = useState('');
 
     useEffect(() => {
         async function loadInitialPosition() {
@@ -30,26 +34,59 @@ function Main({ navigation }) {
         loadInitialPosition();
     }, [])
 
+    async function loadDevs(){
+        const { latitude, longitude } = currentRegion;
+
+        const response = await api.get('/search', {
+            params: {
+                latitude,
+                longitude,
+                techs: techs
+            }
+        });
+
+        setDevs(response.data.devs);
+    }
+
+    function handleRegionChanged(region){
+        setCurrentRegion(region);
+    }
+
     if(!currentRegion) {
         return null;
     }
 
     return (
     <>
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -22.680367, longitude: -47.657567}}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars.githubusercontent.com/u/85360273?v=4'}} />
+        <MapView 
+            onRegionChangeComplete={handleRegionChanged} 
+            initialRegion={currentRegion} 
+            style={styles.map}
+        >
+            {devs.map(dev => (
+                <Marker 
+                    key={dev._id}
+                    coordinate={{ 
+                        longitude: dev.location.coordinates[0],
+                        latitude: dev.location.coordinates[1], 
+                    }}
+            >
+                <Image 
+                    style={styles.avatar} 
+                    source={{ uri: dev.avatar_url }} 
+                />
 
                 <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'WDelesposti'});
+                    navigation.navigate('Profile', { github_username: dev.github_username});
                 }}>
                     <View style={styles.callout}>
-                        <Text style={styles.devName}>Wallace Delesposti</Text>
-                        <Text style={styles.devBio}>Desenvolvedor Junior</Text>
-                        <Text style={styles.devTechs}>Node.js, React</Text>
+                        <Text style={styles.devName}>{dev.name}</Text>
+                        <Text style={styles.devBio}>{dev.bio}</Text>
+                        <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
                     </View>
                 </Callout>
             </Marker>
+            ))}
         </MapView>
     
         <View style={styles.searchForm}>
@@ -58,10 +95,12 @@ function Main({ navigation }) {
                 placeholder="Buscar devs por techs"
                 placeholderTextColor="#999"
                 autoCapitalize="words"
-                autoCorrect={false}            
+                autoCorrect={false}  
+                value={techs}
+                onChangeText={setTechs}          
             />
 
-            <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+            <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
                 <MaterialIcons name="my-location" size={20} color="#FFF" />
             </TouchableOpacity>
         </View>
@@ -101,6 +140,7 @@ const styles = StyleSheet.create({
         marginTop: 5,
     },
 
+    //está no top para economizar tempo, pode pegar os dados do Keyboard para saber quando está sendo usado e quando não
     searchForm: {
         position: 'absolute',
         top: 20, //da pra deixar no bottom, mas precisa mexer na dinâmica do teclado, tem na documentação do react
